339f0d94c37fd24aed444a238e3bbb520678ce8a
KristopherTab <krzysiektab@gmail.com>
2026-02-12 14:03:45 +0100

    generate response in chunks

--- Chatbot.py
+++ Chatbot.py
@@ -7,7 +7,11 @@ st.title("🦜🔗 Langchain Quickstart App")
 def generate_response(input_text):
     llm = ChatOllama(model="gemma2:2b", temperature=0)
     messages = [HumanMessage(content=input_text)]
-    st.info(llm.invoke(messages).content)
+    response_placeholder = st.empty()
+    full_response = ""
+    for chunk in llm.stream(messages):
+        full_response += chunk.content
+        response_placeholder.info(full_response)
 
 with st.form("my_form"):
     text = st.text_area("Enter text:", "What are 3 key advice for learning how to code?")
